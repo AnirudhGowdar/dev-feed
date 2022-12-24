6b53ed7980b64bd0c30fa5ec36aa032447aaf6b3
Anirudh G <anirudhgowdar@gmail.com>
2022-12-25 00:28:35 +0530

    Fixed errors and upgraded packages

--- config/db.js
+++ config/db.js
@@ -1,20 +1,18 @@
-mongoose = require('mongoose');
-const config = require('config');
-var db = '';
-if (process.env.NODE_ENV !== 'production') {
-	db = config.get('mongoURI'); //defined in default.json
+const mongoose = require("mongoose");
+const config = require("config");
+
+mongoose.set("strictQuery", true);
+
+var db = "";
+if (process.env.NODE_ENV !== "production") {
+	db = config.get("mongoURI"); //defined in default.json
 } else {
 	db = process.env.mongoURI;
 }
 const connectDB = async () => {
 	try {
-		await mongoose.connect(db, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-			useFindAndModify: false
-		});
-		console.log('Connected to database');
+		await mongoose.connect(db);
+		console.log("Connected to database");
 	} catch (err) {
 		console.error(err.message);
 		process.exit(1);
